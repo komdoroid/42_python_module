a1087c14fd08399efb4082d7e82faef5194a4e0a
kkomurat <kom.commoc@gmail.com>
2026-06-11 22:53:51 +0900

    update

---
+++ PythonModule00/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,9 @@
+def ft_count_harvest_recursive() -> None:
+    def recursive(days: int) -> None:
+        if (days > 1):
+            recursive(days - 1)
+        print(f"Day {days}")
+
+    days = int(input("Days until harvest: "))
+    recursive(days)
+    print("Harvest time!")
