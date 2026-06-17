f75795103ee2c127373095e239a3146bf84cc98f
kkomurat <kom.commoc@gmail.com>
2026-06-17 22:42:19 +0900

    done ex1

--- PythonModule01/ex0/ft_garden_intro.py
+++ PythonModule01/ex0/ft_garden_intro.py
@@ -1,5 +1,14 @@
 def ft_garden_intro() -> None:
+    name = 'Rose'
+    height = 25
+    age = 30
+    print(f'Plant: {name}')
+    print(f'Height: {height}')
+    print(f'Age: {age}')
+
 
 if __name__ == '__main__':
     print('=== Welcome to My Garden ===')
+    ft_garden_intro()
+    print('')
     print('\n=== End of Program ===')
---
+++ PythonModule01/ex1/ft_garden_data.py
@@ -0,0 +1,19 @@
+class   Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f'{self.name}: {self.height}cm, {self.age} days old')
+
+rose = Plant('Rose', 25, 30)
+sunflower = Plant('Sunflower', 80, 45)
+cactus = Plant('Cactus', 15, 120)
+
+if __name__ == '__main__':
+    print('=== Garden Plant Registry ===')
+    rose.show()
+    sunflower.show()
+    cactus.show()
+    print('=== End of Program ===')
