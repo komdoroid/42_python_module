7bcd3afb95d26561e63f7b8b6a9e2ae5ec53e0c6
komdoroid <kom.commoc@gmail.com>
2026-06-28 15:29:27 +0900

    complete ex4

--- PythonModule02/ex3/ft_custom_errors.py
+++ PythonModule02/ex3/ft_custom_errors.py
@@ -1,20 +1,59 @@
+#! /usr/bin/python3
+
 class GardenError(Exception):
-    pass
+    def __init__(self,
+                 message='Unknown plant error') -> None:
+        super().__init__(message)
+
 
 class PlantError(GardenError):
-    pass
+    def __init__(self,
+                 message='The tomato plant is wilting!') -> None:
+        super().__init__(message)
+
 
 class WaterError(GardenError):
-    pass
+    def __init__(self,
+                 message='Not enough water in the tank!') -> None:
+        super().__init__(message)
 
-def test_custom_errors() -> None:
-    pass
+
+def test_custom_errors(test_error_num: int) -> None:
+    try:
+        if test_error_num == 0:
+            raise PlantError()
+        if test_error_num == 1:
+            raise WaterError()
+    except PlantError as e:
+        print(f'Caught PlantError: {e}')
+    except WaterError as e:
+        print(f'Caught WaterError: {e}')
+
+
+def test_garden_error(test_error_num: int) -> None:
+    try:
+        if test_error_num == 0:
+            raise PlantError()
+        if test_error_num == 1:
+            raise WaterError()
+    except GardenError as e:
+        print(f'Caught GardenError: {e}')
+
 
 if __name__ == '__main__':
     print('=== Custom Garden Errors Demo ===\n')
 
     print('Testing PlnatError...')
+    test_custom_errors(0)
+    print()
+
     print('Testing WaterError...')
+    test_custom_errors(1)
+    print()
+
     print('Testing catching all garden errors...')
+    test_garden_error(0)
+    test_garden_error(1)
+    print()
 
     print('All custom error types work correctly!')
---
+++ PythonModule02/ex4/ft_finally_block.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python3
+
+class GardenError(Exception):
+    def __init__(self,
+                 message='Unknown plant error') -> None:
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self,
+                 message='The tomato plant is wilting!') -> None:
+        super().__init__(message)
+
+
+def water_plant(plant_name: str) -> None:
+    if plant_name == plant_name.capitalize():
+        print(f'Watering {plant_name}: [OK]')
+    else:
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+
+
+def test_watering_system(plant_list: list[str]) -> None:
+    try:
+        print('Opening watering system')
+        for plant in plant_list:
+            water_plant(plant)
+    except PlantError as e:
+        print(f'Caught PlantError: {e}\n'
+              f'.. ending tests and returning to main')
+    finally:
+        print('Closing watering system')
+
+
+if __name__ == '__main__':
+    print('=== Garden Watering System ===\n')
+
+    print('Testing valid plants...')
+    test_watering_system(['Tomato', 'Lettuce', 'Carrots'])
+    print()
+
+    print('Testing invalid plants...')
+    test_watering_system(['Tomato', 'lettuce', 'Carrots'])
+    print()
+
+    print('Cleanup always happens, even with errors!')
