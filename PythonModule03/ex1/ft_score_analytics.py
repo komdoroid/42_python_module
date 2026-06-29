74be8c053dac60c8141814fe19a82db0ea4b4154
kkomurat <kom.commoc@gmail.com>
2026-06-29 23:08:41 +0900

    update module03

--- PythonModule03/ex1/ft_score_analytics.py
+++ PythonModule03/ex1/ft_score_analytics.py
@@ -2,10 +2,29 @@
 
 import sys
 
+def calculate_score(args: list[str]) -> None:
+    scores = []
+
+    for score in args:
+        try:
+            scores.append(int(score))
+        except:
+            print("Invalid parameter: '{score}'")
+    scores_num = len(scores)
+    if scores_num == 0:
+        print('No scores provided. '
+              'Usage: python3 ft_score_analytics.py '
+              '<score1> <score2> ...')
+    else:
+        print(f'Total players: {scores_num}\n'
+              f'Total score: {sum(scores)}\n'
+              f'Average score: {sum(scores) / scores_num}\n'
+              f'High score: {max(scores)}\n'
+              f'Low score: {min(scores)}\n'
+              f'Score range: {max(scores) - min(scores)}\n'
+              )
+
 
 if __name__ == '__main__':
-    scores = sys.argv[1:]
-
     print('=== Player Score Analytics ===')
-
-    print(f'Scores processed: {scores}')
+    calculate_score(sys.argv[1:])
