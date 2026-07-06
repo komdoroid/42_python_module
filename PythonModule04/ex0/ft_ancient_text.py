5aa87dbc7bd88f80a1a7e16f6d897deb7dc6b6b2
kkomurat <kom.commoc@gmail.com>
2026-07-06 23:02:21 +0900

    update mo04

--- PythonModule04/ex0/ft_ancient_text.py
+++ PythonModule04/ex0/ft_ancient_text.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+import sys
+from typing import IO
+
+
+def output_ancient_text(target_file: str) -> IO[str]:
+    f = open(target_file, 'r')
+    print('---\n')
+    print(f'{f.read()}')
+    print('---')
+    return f
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Usage {sys.argv[0]} <file>')
+    else:
+        target_file = sys.argv[1]
+        try:
+            print('=== Cyber Archives Recovery ===')
+            print(f"Accessing file '{target_file}'")
+            f = output_ancient_text(target_file)
+            print(f"File '{target_file}' closed.")
+        except FileNotFoundError as e:
+            print(f"Error opening file '{target_file}': {e}")
+        except PermissionError as e:
+            print(f"Error opening file '{target_file}': {e}")
+        finally:
+            f.close()
--- PythonModule04/ex1/ft_archive_creation.py
+++ PythonModule04/ex1/ft_archive_creation.py
@@ -1,1 +1,50 @@
 #!/usr/bin/env python3
+
+import sys
+from typing import IO
+
+
+def output_ancient_text(target_file: str) -> IO[str]:
+    f = open(target_file, 'r')
+    print('---\n')
+    print(f'{f.read()}')
+    print('---')
+    return f
+
+
+def add_ancient_text(target_file: str) -> IO[str]:
+    f = open(target_file, 'a')
+    f.write('#')
+    f.close()
+    print('---\n')
+    f = open(target_file, 'r')
+    print(f'{f.read()}')
+    print('---\n')
+    return f
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Usage {sys.argv[0]} <file>')
+    else:
+        target_file = sys.argv[1]
+        try:
+            print('=== Cyber Archives Recovery ===')
+            print(f"Accessing file '{target_file}'")
+            f = output_ancient_text(target_file)
+            print(f"File '{target_file}' closed.\n")
+        except FileNotFoundError as e:
+            print(f"Error opening file '{target_file}': {e}")
+        except PermissionError as e:
+            print(f"Error opening file '{target_file}': {e}")
+        finally:
+            f.close()
+
+        try:
+            print('Transform data:')
+            f = add_ancient_text(target_file)
+            input('Enter new file name (or empty): ')
+        except Exception as e:
+            print(e)
+        finally:
+            f.close()
