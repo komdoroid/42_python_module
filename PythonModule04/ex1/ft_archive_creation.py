1893fe7b84f054293b8d5a11e1c043977d5a35c7
kkomurat <kom.commoc@gmail.com>
2026-07-08 23:02:51 +0900

    update ex1

--- PythonModule04/ex1/ft_archive_creation.py
+++ PythonModule04/ex1/ft_archive_creation.py
@@ -4,23 +4,33 @@ import sys
 from typing import IO
 
 
-def output_ancient_text(target_file: str) -> IO[str]:
+def output_ancient_text(target_file: str) -> str:
     f = open(target_file, 'r')
+    content: str = f.read()
+    return content
+
+
+def add_ancient_text(content: str) -> str:
+    lines = content.splitlines()
+    new_content = "\n".join([line + '#' for line in lines])
+    return new_content
+
+
+def transform_data_procedure(target_file: str) -> str:
+    print('=== Cyber Archives Recovery ===')
     print('---\n')
-    print(f'{f.read()}')
+    print(f"Accessing file '{target_file}'")
     print('---')
-    return f
+    content = output_ancient_text(target_file)
+    print(content)
+    print(f"File '{target_file}' closed.\n")
 
-
-def add_ancient_text(target_file: str) -> IO[str]:
-    f = open(target_file, 'a')
-    f.write('#')
-    f.close()
+    print('Transform data:')
     print('---\n')
-    f = open(target_file, 'r')
-    print(f'{f.read()}')
-    print('---\n')
-    return f
+    new_content = add_ancient_text(content)
+    print(new_content)
+    print('---')
+    return new_content
 
 
 if __name__ == '__main__':
@@ -29,22 +39,18 @@ if __name__ == '__main__':
     else:
         target_file = sys.argv[1]
         try:
-            print('=== Cyber Archives Recovery ===')
-            print(f"Accessing file '{target_file}'")
-            f = output_ancient_text(target_file)
-            print(f"File '{target_file}' closed.\n")
+            new_content = transform_data_procedure(target_file)
+            new_file_name = input('Enter new file name (or empty): ')
+            if not new_file_name:
+                print('Not saving data.')
+            else:
+                print(f"Saving data to '{new_file_name}'")
+                print(f"Data saved in file '{new_file_name}'")
+                f = open(new_file_name, 'w')
+                f.write(new_content)
         except FileNotFoundError as e:
             print(f"Error opening file '{target_file}': {e}")
         except PermissionError as e:
             print(f"Error opening file '{target_file}': {e}")
-        finally:
-            f.close()
-
-        try:
-            print('Transform data:')
-            f = add_ancient_text(target_file)
-            input('Enter new file name (or empty): ')
-        except Exception as e:
-            print(e)
         finally:
             f.close()
