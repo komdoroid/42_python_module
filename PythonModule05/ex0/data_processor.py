0faac5bff670a57976c8678986a3aaf4207f2d98
kkomurat <kom.commoc@gmail.com>
2026-07-12 18:38:48 +0900

    update

--- PythonModule05/ex0/data_processor.py
+++ PythonModule05/ex0/data_processor.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env/ python3
 
 import typing
-import abc
+from abc import ABC, abstractmethod
 
 
-class DataProcessor:
-    def validate(self, data: Any) -> bool:
+class DataProcessor(ABC):
+    data_list = []
+    validated = []
+
+    @abstractmethod
+    def validate(self, data: any) -> bool:
         pass
 
 
+    @abstractmethod
     def inget(self, ) -> None:
         pass
 
@@ -17,8 +22,25 @@ class DataProcessor:
         pass
 
 
-class NumericProcessor:
-    def validate() -> None:
+class NumericProcessor(DataProcessor):
+    def validate(self, data) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        if isinstance(data, list):
+            return all(isinstance(item, (int, float)) for item in data)
+        return False
+
+
+    def inget(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
+            print(f"'{data}' without prior validation:", end='')
+            print('Got exception: Improper numeric data')
+            return
+        self.data_list.append(str(data))
+
+
+class TextProcessor(DataProcessor):
+    def validate(self, data:any) -> bool:
         pass
 
 
@@ -26,17 +48,8 @@ class NumericProcessor:
         pass
 
 
-class TextProcessor:
-    def validate() -> None:
-        pass
-
-
-    def inget() -> None:
-        pass
-
-
-class LogProcessor:
-    def validate() -> None:
+class LogProcessor(DataProcessor):
+    def validate(self, data: any) -> bool:
         pass
 
 
@@ -45,7 +58,18 @@ class LogProcessor:
 
 
 if __name__ == '__main__':
+    numProcessor = NumericProcessor()
+    textProcessor = TextProcessor()
+    logProcessor = LogProcessor()
+    num_data = 42
+    str_data = 'Hello'
+    foo_data = 'foo'
+
     print('=== Code Nexus - Data Processor ===\n')
 
     print('Testing Numeric Processor...')
-
+    print(f" Trying to validate input '{num_data}'"
+          f": {numProcessor.validate(num_data)}")
+    print(f" Trying to validate input '{str_data}'"
+          f": {numProcessor.validate(num_data)}")
+    print(f"Test invalid ingestion of string {numProcessor.inget(foo_data)}")
