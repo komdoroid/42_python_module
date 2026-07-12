54a93b2a1ed951047b2a97b29772d9a941741d04
kkomurat <kom.commoc@gmail.com>
2026-07-12 21:32:07 +0900

    update mo05/ex0

--- PythonModule05/ex0/data_processor.py
+++ PythonModule05/ex0/data_processor.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env/ python3
+#!/usr/bin/env/ python3
 
 import typing
 from abc import ABC, abstractmethod
@@ -6,7 +6,7 @@ from abc import ABC, abstractmethod
 
 class DataProcessor(ABC):
     data_list = []
-    validated = []
+    rank_counter = 0
 
     @abstractmethod
     def validate(self, data: any) -> bool:
@@ -19,7 +19,9 @@ class DataProcessor(ABC):
 
 
     def output(self) -> tuple[int, str]:
-        pass
+        now_rank = self.rank_counter
+        self.rank_counter += 1
+        return now_rank, self.data_list.pop(0)
 
 
 class NumericProcessor(DataProcessor):
@@ -33,28 +35,56 @@ class NumericProcessor(DataProcessor):
 
     def inget(self, data: int | float | list[int | float]) -> None:
         if not self.validate(data):
-            print(f"'{data}' without prior validation:", end='')
+            print(f"'{data}' without prior validation:")
             print('Got exception: Improper numeric data')
             return
-        self.data_list.append(str(data))
+        if isinstance(data, list):
+            for i in range(len(data)):
+                self.data_list.append(str(data[i]))
+        else:
+            self.data_list.append(str(data))
 
 
 class TextProcessor(DataProcessor):
-    def validate(self, data:any) -> bool:
-        pass
+    def validate(self, data: any) -> bool:
+        if isinstance(data, str):
+            return True
+        if isinstance(data, list):
+            return all(isinstance(item, str) for item in data)
+        return False
 
 
-    def inget() -> None:
-        pass
+    def inget(self, data: str | list[str]) -> None:
+        if not self.validate(data):
+            print(f"'{data}' without prior validation:")
+            print('Got exception: Improper text data')
+            return
+        if isinstance(data, list):
+            for i in range(len(data)):
+                self.data_list.append(str(data[i]))
+        else:
+            self.data_list.append(str(data))
 
 
 class LogProcessor(DataProcessor):
     def validate(self, data: any) -> bool:
-        pass
+        if isinstance(data, dict):
+            return True
+        if isinstance(data, list):
+            return all(isinstance(item, dict) for item in data)
+        return False
 
 
     def inget() -> None:
-        pass
+        if not self.validate(data):
+            print(f"'{data}' without prior validation:")
+            print('Got exception: Improper log data')
+            return
+        if isinstance(data, list):
+            for i in range(len(data)):
+                self.data_list.append(str(data[i]))
+        else:
+            self.data_list.append(str(data))
 
 
 if __name__ == '__main__':
@@ -64,6 +94,8 @@ if __name__ == '__main__':
     num_data = 42
     str_data = 'Hello'
     foo_data = 'foo'
+    num_data_list = [1, 2, 3, 4, 5]
+    str_data_list = ['Hello', 'Nexus', 'World']
 
     print('=== Code Nexus - Data Processor ===\n')
 
@@ -72,4 +104,27 @@ if __name__ == '__main__':
           f": {numProcessor.validate(num_data)}")
     print(f" Trying to validate input '{str_data}'"
           f": {numProcessor.validate(num_data)}")
-    print(f"Test invalid ingestion of string {numProcessor.inget(foo_data)}")
+    print("Test invalid ingestion of string ", end='')
+    numProcessor.inget(foo_data)
+    print(f"Processing data: {num_data_list}")
+    loop_count = 3
+    print(f"Extracting {loop_count} values...")
+    if numProcessor.validate(num_data_list):
+        numProcessor.inget(num_data_list)
+    for i in range(loop_count):
+        rank_counter, pop_data = numProcessor.output()
+        print(f"Numeric value {rank_counter}: {pop_data}")
+    print()
+
+    print('Testing Text Processor...')
+    print(f" Trying to validate input '{num_data}'"
+          f": {textProcessor.validate(num_data)}")
+    print(f"Processing data: {str_data_list}")
+    loop_count = 1
+    print(f"Extracting {loop_count} values...")
+    if textProcessor.validate(str_data_list):
+        textProcessor.inget(str_data_list)
+    for i in range(loop_count):
+        rank_counter, pop_data = textProcessor.output()
+        print(f"Text value {rank_counter}: {pop_data}")
+    print()
