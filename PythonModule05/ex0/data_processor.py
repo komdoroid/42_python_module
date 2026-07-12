3871beecb8179bd388f4e6af67d747c627133f62
kkomurat <kom.commoc@gmail.com>
2026-07-12 13:53:16 +0900

    20260712

---
+++ PythonModule05/ex0/data_processor.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env/ python3
+
+import typing
+import abc
+
+
+class DataProcessor:
+    def validate(self, data: Any) -> bool:
+        pass
+
+
+    def inget(self, ) -> None:
+        pass
+
+
+    def output(self) -> tuple[int, str]:
+        pass
+
+
+class NumericProcessor:
+    def validate() -> None:
+        pass
+
+
+    def inget() -> None:
+        pass
+
+
+class TextProcessor:
+    def validate() -> None:
+        pass
+
+
+    def inget() -> None:
+        pass
+
+
+class LogProcessor:
+    def validate() -> None:
+        pass
+
+
+    def inget() -> None:
+        pass
+
+
+if __name__ == '__main__':
+    print('=== Code Nexus - Data Processor ===\n')
+
+    print('Testing Numeric Processor...')
+
