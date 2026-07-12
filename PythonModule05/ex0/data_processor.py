c6d77e313aae58ccef1f69cd308ae97be80bdb53
kkomurat <kom.commoc@gmail.com>
2026-07-12 22:24:48 +0900

    last update 20260712

--- PythonModule05/ex0/data_processor.py
+++ PythonModule05/ex0/data_processor.py
@@ -5,8 +5,9 @@ from abc import ABC, abstractmethod
 
 
 class DataProcessor(ABC):
-    data_list = []
-    rank_counter = 0
+    def __init__(self) -> None:
+        self.data_list = []
+        self.rank_counter = 0
 
     @abstractmethod
     def validate(self, data: any) -> bool:
@@ -36,7 +37,7 @@ class NumericProcessor(DataProcessor):
     def inget(self, data: int | float | list[int | float]) -> None:
         if not self.validate(data):
             print(f"'{data}' without prior validation:")
-            print('Got exception: Improper numeric data')
+            print(' Got exception: Improper numeric data')
             return
         if isinstance(data, list):
             for i in range(len(data)):
@@ -75,16 +76,17 @@ class LogProcessor(DataProcessor):
         return False
 
 
-    def inget() -> None:
+    def inget(self, data: dict | list[dict]) -> None:
         if not self.validate(data):
             print(f"'{data}' without prior validation:")
             print('Got exception: Improper log data')
             return
         if isinstance(data, list):
-            for i in range(len(data)):
-                self.data_list.append(str(data[i]))
+            for item in data:
+                result = ': '.join(item.values())
+                self.data_list.append(result)
         else:
-            self.data_list.append(str(data))
+            self.data_list.append(str(data.values()))
 
 
 if __name__ == '__main__':
@@ -96,6 +98,10 @@ if __name__ == '__main__':
     foo_data = 'foo'
     num_data_list = [1, 2, 3, 4, 5]
     str_data_list = ['Hello', 'Nexus', 'World']
+    dict_data_list = [{'log_level': 'NOTICE',
+                  'log_message': 'Connection to server'},
+                 {'log_level': 'ERROR',
+                  'log_message': 'Unauthorized access!!'}]
 
     print('=== Code Nexus - Data Processor ===\n')
 
@@ -103,28 +109,40 @@ if __name__ == '__main__':
     print(f" Trying to validate input '{num_data}'"
           f": {numProcessor.validate(num_data)}")
     print(f" Trying to validate input '{str_data}'"
-          f": {numProcessor.validate(num_data)}")
-    print("Test invalid ingestion of string ", end='')
+          f": {numProcessor.validate(str_data)}")
+    print(" Test invalid ingestion of string ", end='')
     numProcessor.inget(foo_data)
-    print(f"Processing data: {num_data_list}")
+    print(f" Processing data: {num_data_list}")
     loop_count = 3
-    print(f"Extracting {loop_count} values...")
+    print(f" Extracting {loop_count} values...")
     if numProcessor.validate(num_data_list):
         numProcessor.inget(num_data_list)
     for i in range(loop_count):
         rank_counter, pop_data = numProcessor.output()
-        print(f"Numeric value {rank_counter}: {pop_data}")
+        print(f" Numeric value {rank_counter}: {pop_data}")
     print()
 
     print('Testing Text Processor...')
     print(f" Trying to validate input '{num_data}'"
           f": {textProcessor.validate(num_data)}")
-    print(f"Processing data: {str_data_list}")
+    print(f" Processing data: {str_data_list}")
     loop_count = 1
-    print(f"Extracting {loop_count} values...")
+    print(f" Extracting {loop_count} values...")
     if textProcessor.validate(str_data_list):
         textProcessor.inget(str_data_list)
     for i in range(loop_count):
         rank_counter, pop_data = textProcessor.output()
-        print(f"Text value {rank_counter}: {pop_data}")
+        print(f" Text value {rank_counter}: {pop_data}")
     print()
+
+    print('Testing Log Processor...')
+    print(f" Trying to validate input '{str_data}'"
+          f": {logProcessor.validate(str_data)}")
+    print(f" Processing data: {dict_data_list}")
+    loop_count = 2
+    print(f" Extracting {loop_count} values...")
+    if logProcessor.validate(dict_data_list):
+        logProcessor.inget(dict_data_list)
+    for i in range(loop_count):
+        rank_counter, pop_data = logProcessor.output()
+        print(f" Log {rank_counter}: {pop_data}")
