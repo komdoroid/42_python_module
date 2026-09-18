af82749bf268a676fbcea5e0c60b5d862af6c63f
kkomurat <kom.commoc@gmail.com>
2026-09-18 22:29:41 +0900

    ex0 done

---
+++ PythonModule08/ex0/construct.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import site
+
+if __name__ == '__main__':
+    # print(os.environ)
+    if sys.prefix == sys.base_prefix:
+        # Outside the Matrix
+        print("MATRIX_STATUS: You're still pluged in")
+
+        print(f"Current Python: {sys.executable}")
+        print("Virtual Environment: None detected\n")
+
+        print("WARNING: You're in the global environment!")
+        print("The machines can see everything you install.\n")
+
+        print("To enter the construct, run:")
+        print("python -m venv matrix_env")
+        print("source matrix_env/bin/activate # On Unix")
+        print("matrix_env\Scripts\activate # On Windows\n")
+
+        print("Then run this program again.")
+
+    else:
+        # Inside the Construct
+        print("MATRIX STATUS: Welcome to the construct\n")
+        
+        print(f"Current Python: {sys.executable}")
+        print(f"Virtual Environment: {os.path.basename(sys.prefix)}")
+        print(f"Environment Path: {sys.prefix}\n")
+
+        print("SUCCESS: You're in an isolated environment!")
+        print("Safe to install packages without affecting")
+        print("the global system.\n")
+
+        print("Package installation path:")
+        print(f"{site.getsitepackages()[0]}")
